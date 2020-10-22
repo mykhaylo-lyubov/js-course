712cd91d43e8860e90ec98479baa98bbc86933db
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2020-10-22 10:41:12 +0300

    continue with study of JS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const valueA = '5';
+console.log(valueA);
+console.log(Number(valueA));
+console.log(typeof Number(valueA));
+
+const valueB = 'random string';
+console.log(Number(valueB));
+console.log(typeof Number(valueB));
