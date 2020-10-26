6f59da5b8eee8b5fd8ad2ebabe10c1bf08366306
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2020-10-26 17:32:42 +0200

    wow

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ const countryInputRef = document.querySelector('input[name = "country"]');
 const buttonRef = document.querySelector('button');
 
 buttonRef.addEventListener('click', function () {
-  //   console.log('country - ', countryInputRef.value);
   if (countryInputRef.value === null) {
     message = CANCELED_BY_USER;
   } else {
@@ -49,31 +48,3 @@ buttonRef.addEventListener('click', function () {
 
   console.log(message);
 });
-
-//  if (countryName === null) {
-//    message = CANCELED_BY_USER;
-//  } else {
-//    country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
-//    switch (country) {
-//      case CHINA:
-//        price = 100;
-//        break;
-//      case AUSTRALIA:
-//        price = 170;
-//        break;
-//      case INDIA:
-//        price = 80;
-//        break;
-//      case JAMAICA:
-//        price = 120;
-//        break;
-//      default:
-//        message = NO_DELIVERY;
-//    }
-//  }
-
-//  if (price > 0) {
-//    message = `Доставка в ${country} будет стоить ${price} кредитов`;
-//  }
-
-//  console.log(message);
