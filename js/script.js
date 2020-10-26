e02c1326349a677fae3c30914a90b2734cefad54
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2020-10-26 17:29:36 +0200

    home task no 1.5 was made

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const countryName = 'КитаЙ';
+// const countryName = 'КитаЙ';
 
 const CANCELED_BY_USER = 'Отменено пользователем!';
 const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
@@ -12,30 +12,68 @@ let message;
 let price;
 let country;
 
-if (countryName === null) {
-  message = CANCELED_BY_USER;
-} else {
-  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
-  switch (country) {
-    case CHINA:
-      price = 100;
-      break;
-    case AUSTRALIA:
-      price = 170;
-      break;
-    case INDIA:
-      price = 80;
-      break;
-    case JAMAICA:
-      price = 120;
-      break;
-    default:
-      message = NO_DELIVERY;
+const countryInputRef = document.querySelector('input[name = "country"]');
+const buttonRef = document.querySelector('button');
+
+buttonRef.addEventListener('click', function () {
+  //   console.log('country - ', countryInputRef.value);
+  if (countryInputRef.value === null) {
+    message = CANCELED_BY_USER;
+  } else {
+    country =
+      countryInputRef.value[0].toUpperCase() +
+      countryInputRef.value.slice(1).toLowerCase();
+    switch (country) {
+      case CHINA:
+        price = 100;
+        break;
+      case AUSTRALIA:
+        price = 170;
+        break;
+      case INDIA:
+        price = 80;
+        break;
+      case JAMAICA:
+        price = 120;
+        break;
+      default:
+        price = 0;
+        alert(NO_DELIVERY);
+    }
   }
-}
 
-if (price > 0) {
-  message = `Доставка в ${country} будет стоить ${price} кредитов`;
-}
+  if (price > 0) {
+    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+    alert((message = `Доставка в ${country} будет стоить ${price} кредитов`));
+  }
 
-console.log(message);
+  console.log(message);
+});
+
+//  if (countryName === null) {
+//    message = CANCELED_BY_USER;
+//  } else {
+//    country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase();
+//    switch (country) {
+//      case CHINA:
+//        price = 100;
+//        break;
+//      case AUSTRALIA:
+//        price = 170;
+//        break;
+//      case INDIA:
+//        price = 80;
+//        break;
+//      case JAMAICA:
+//        price = 120;
+//        break;
+//      default:
+//        message = NO_DELIVERY;
+//    }
+//  }
+
+//  if (price > 0) {
+//    message = `Доставка в ${country} будет стоить ${price} кредитов`;
+//  }
+
+//  console.log(message);
