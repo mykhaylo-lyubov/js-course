da015fbc72ebfdc2cd746290a5dc10980085f9c7
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2020-11-04 12:26:19 +0200

    module 2 task 2 is done

---
+++ js/hw-02-02.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// const calculateEngravingPrice = function (message = '', pricePerWord = 0) {
+//   pricePerWord = Number(message.split(' ').push()) * pricePerWord;
+//   return pricePerWord;
+// };
+
+const calculateEngravingPrice = (message = '', pricePerWord = 0) =>
+  (pricePerWord = Number(message.split(' ').length) * pricePerWord);
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+); // 160
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
+
+console.log(calculateEngravingPrice('Uno', 100)); // 100
