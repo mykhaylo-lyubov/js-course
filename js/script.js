ca2d3f15ee6c388d1a405fe4bb9264a43d534925
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2020-11-04 10:01:38 +0200

    module 2 task 1 done

--- js/script.js
+++ js/script.js
@@ -1,50 +1,15 @@
-'use strict';
+// 'use strict';
 
-// const countryName = 'КитаЙ';
-
-const CANCELED_BY_USER = 'Отменено пользователем!';
-const NO_DELIVERY = 'В выбранную страну доставка недоступна.';
-const CHINA = 'Китай';
-const AUSTRALIA = 'Австралия';
-const INDIA = 'Индия';
-const JAMAICA = 'Ямайка';
-let message;
-let price;
-let country;
-
-const countryInputRef = document.querySelector('input[name = "country"]');
-const buttonRef = document.querySelector('button');
-
-buttonRef.addEventListener('click', function () {
-  if (countryInputRef.value === null) {
-    message = CANCELED_BY_USER;
-  } else {
-    country =
-      countryInputRef.value[0].toUpperCase() +
-      countryInputRef.value.slice(1).toLowerCase();
-    switch (country) {
-      case CHINA:
-        price = 100;
-        break;
-      case AUSTRALIA:
-        price = 170;
-        break;
-      case INDIA:
-        price = 80;
-        break;
-      case JAMAICA:
-        price = 120;
-        break;
-      default:
-        price = 0;
-        alert(NO_DELIVERY);
-    }
+const getItemsString = function (array) {
+  'use strict';
+  let result;
+  for (let i = 0; i < array.length; i += 1) {
+    result = `${i + 1} - ${array[i]}`;
   }
+  return result;
+};
 
-  if (price > 0) {
-    message = `Доставка в ${country} будет стоить ${price} кредитов`;
-    alert((message = `Доставка в ${country} будет стоить ${price} кредитов`));
-  }
+getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+// console.log(result);
 
-  console.log(message);
-});
+// console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
