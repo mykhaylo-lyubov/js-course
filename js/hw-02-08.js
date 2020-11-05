c6caec5d43f5d264d8184f7c441d08829afedba5
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2020-11-05 14:21:40 +0200

    module 2 task no 8 is done

---
+++ js/hw-02-08.js
@@ -0,0 +1,22 @@
+function reduceArray(array) {
+  'use strict';
+  let total = 0;
+
+  if (array.length !== 0) {
+    for (const element of array) {
+      total += element;
+    }
+  }
+
+  return total;
+}
+
+console.log(reduceArray([1, 2, 3]));
+// 6
+
+console.log(reduceArray([-2, 0, 2]));
+// 0
+
+console.log(reduceArray([1, 2, 2.5]));
+// 5.5
+console.log(reduceArray([]));
